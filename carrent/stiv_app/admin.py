f0a2569aa76ca49661657fd9872bb31356a8f42c
Stephen Mwangi <tudysteve@gmail.com>
2026-04-22 20:39:10 +0300

    final css fix

--- carrent/stiv_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Car  # <-- import the model
-
-# Register the Car model so it appears in the admin
-admin.site.register(Car)
-# Register your models here.
--- carrent/stiv_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StivConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stiv_app'
